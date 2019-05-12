f5b2f0d913887574929d50e506baf441fc4b98ee
Ollie Smith <ollie.smith131@btinternet.com>
2019-05-12 13:20:01 +0100

    Adding solution to 'Exceptions'

---
+++ python_random/exceptions.py
@@ -0,0 +1,10 @@
+T = int(input())
+
+for _ in range(T):
+    a, b = input().split()
+    try:
+       print(int(a)//int(b))
+    except ZeroDivisionError as z:
+        print('Error Code:', z)
+    except ValueError as v:
+        print('Error Code:', v)
